817c2ed94a7cdba74d943755f29132886fd3ae68
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-12 22:24:18 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/Pages/About/About';
+import Blog from './Components/Pages/Blog/Blog';
+import Contacts from './Components/Pages/Contacts/Contacts';
 import Home from './Components/Pages/Home/Home';
-import Route from './Components/Pages/Home/Home';
+import Pricing from './Components/Pages/Pricing/Pricing';
+import Shop from './Components/Pages/Shop/Shop';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
 
@@ -9,9 +13,15 @@ function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
       <Routes>
-        {/* <Route to='/home' element={<Route></Route>}></Route> */}
+        <Route to='/' element={<Home></Home>}></Route>
+        <Route to='/home' element={<Home></Home>}></Route>
+        <Route to='/about' element={<About></About>}></Route>
+        <Route to='/blog' element={<Blog></Blog>}></Route>
+        <Route to='/contact' element={<Contacts></Contacts>}></Route>
+        <Route to='/pricing' element={<Pricing></Pricing>}></Route>
+        <Route to='/services' element={<Pricing></Pricing>}></Route>
+        <Route to='/shop' element={<Shop></Shop>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -11,13 +11,13 @@ const Header = () => {
                     <Navbar.Brand href="#home"> <img style={{ height: '35px' }} src={logo} alt="" /> </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link href="#features">Home</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav className="ms-auto text-white">
+                            <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#features">Features</Nav.Link>
                             <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link href="#blog">Blog</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
