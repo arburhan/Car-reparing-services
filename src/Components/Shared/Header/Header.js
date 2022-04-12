58b5c65afd640c54740ae5bbb1b5967e020903e3
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-12 11:53:10 +0600

    added react bootstrap

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container, Nav } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
     );
 };
 
