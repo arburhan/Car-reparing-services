cab25ccc5af15ac84a247846065cdfbaa90ea64f
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-13 17:34:02 +0600

    work with api secret code

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA2B7xexDWmol0Pp7mkrHQxL9LMXAaGWtA",
-    authDomain: "car-repair-services-85293.firebaseapp.com",
-    projectId: "car-repair-services-85293",
-    storageBucket: "car-repair-services-85293.appspot.com",
-    messagingSenderId: "456519262210",
-    appId: "1:456519262210:web:1358ab63bcdf6f2ac9071b"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
