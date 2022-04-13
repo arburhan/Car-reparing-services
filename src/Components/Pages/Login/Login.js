4ed100113dcede1ac3106c9e79e0fe24a48a0c29
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-13 13:03:30 +0600

    ref added on form

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form'
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const handaleFormSubmit = e => {
+        e.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+    }
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-center text-primary mt-4' >Log in</h2>
             <div>
-                <Form>
+                <Form onSubmit={handaleFormSubmit} >
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -18,7 +26,7 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
