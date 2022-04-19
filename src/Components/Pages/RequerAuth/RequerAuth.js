f1b4a5f119631316c68c23dd6424633ca7a2fe04
arburhan <buthan190629@outlook.com>
2022-04-19 23:15:46 +0600

    verify system added

--- src/Components/Pages/RequerAuth/RequerAuth.js
+++ src/Components/Pages/RequerAuth/RequerAuth.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequerAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
     if (loading) {
         return <Loading></Loading>
     }
     if (!user) {
         return <Navigate to='/login' state={{ from: location }} replace />
+    }
+    if (!user.emailVerified) {
+        return <div className="text-danger text-center">
+            <h3>Your email isn't verify</h3>
+            <p>Please verify your email first</p>
+            <button className='rounded-pill px-3 py-1 text-dark' onClick={async () => {
+                await sendEmailVerification();
+                alert('mail sent');
+
+            }} >Send mail again</button>
+        </div>
     }
 
     return children;
