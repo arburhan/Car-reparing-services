baa91d2803780046524e47792169d1a82c4eb3c5
arburhan <buthan190629@outlook.com>
2022-04-30 09:06:52 +0600

    work on add service

--- src/Components/Pages/AddService/AddService.js
+++ src/Components/Pages/AddService/AddService.js
@@ -2,22 +2,35 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 
 const AddService = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/service`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+            })
+    };
 
-    console.log(watch("example"));
     return (
         <div>
             <div className='w-50 mx-auto'>
                 <h2>Please add services</h2>
                 <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("Name", { required: true, maxLength: 20 })} />
-                    <input {...register("Description")} />
-                    <input type="number" {...register("Price", { required: true })} />
-                    <input type="submit" />
+                    <input placeholder='Name' className='mb-2' {...register("name", { required: true, maxLength: 20 })} />
+                    <textarea placeholder='Description' className='mb-2' {...register("description")} />
+                    <input placeholder='Price' className='mb-2' type="number" {...register("price", { required: true })} />
+                    <input placeholder='Photo URL' className='mb-2' type="text" {...register("img", { required: true })} />
+                    <input type="submit" value="Add Service" />
                 </form>
             </div>
-
         </div>
     );
 };
